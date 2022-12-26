55f76c818a3fab8547b716d0fad5437be64c6f15
VitalyTaran <79220120+VitalyTaran@users.noreply.github.com>
2022-12-26 19:55:02 +0300

    Initial Commit

---
+++ Tips Calculator/Tips_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tips_CalculatorApp.swift
+//  Tips Calculator
+//
+//  Created by Виталий Таран on 26.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct Tips_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
