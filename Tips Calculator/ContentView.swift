8cede511bd689e1f5f8f174274f2bbd5d6af7431
VitalyTaran <79220120+VitalyTaran@users.noreply.github.com>
2022-12-26 20:30:24 +0300

    add Picker and ForEach

--- Tips Calculator/ContentView.swift
+++ Tips Calculator/ContentView.swift
@@ -10,12 +10,18 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
                     TextField("Amount", text: $checkAmount)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
                 }
             }
             .navigationTitle("Калькулятор чаевых")
